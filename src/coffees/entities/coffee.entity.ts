b6bba85d053f95d08d57d1010111018ef50d4305
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-10-08 23:08:00 +0700

    Diving into GraphQL 🐈 - More on Types

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,8 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Coffee {
+  @Field(() => ID, { description: 'A unique identifier' })
   id: number;
   name: string;
   brand: string;
