8447b4f6d3405afc42c30c5ffd82c7fa87b74edb
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-10-08 23:13:11 +0700

    Diving into GraphQL 🐈 - Passing Arguments: Returning a Coffee by ID

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,11 +1,17 @@
-import { Query } from '@nestjs/graphql';
+import { Args, ID, Query } from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
 import { Coffee } from './entities/coffee.entity';
+import { ParseIntPipe } from '@nestjs/common';
 
 @Resolver()
 export class CoffeesResolver {
   @Query(() => [Coffee], { name: 'coffees' })
   async findAll() {
     return [];
+  }
+
+  @Query(() => Coffee, { name: 'coffee', nullable: true })
+  async findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
+    return null;
   }
 }
