95705d9f4d8cdc3d7b309ac119db2f2f0f924168
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-10-14 13:33:02 +0700

    More advanced GraphQL features - Using Field Resolvers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,9 +4,10 @@ import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { CoffeeFlavorsResolver } from './coffee-flavors.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
-  providers: [CoffeesResolver, CoffeesService],
+  providers: [CoffeesResolver, CoffeesService, CoffeeFlavorsResolver],
 })
 export class CoffeesModule {}
